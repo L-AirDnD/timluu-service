079a4b2850ffb0a98805724ecd163f2d77d3c3a0
Tim Luu <timbo.luu@gmail.com>
2019-02-07 14:16:14 -0800

    Add schema outline

---
+++ schema.sql
@@ -0,0 +1,43 @@
+/**************************************
+***                                 ***
+***    DATABASE & TABLE CREATION    ***
+***                                 ***
+***************************************/
+
+CREATE DATABASE lairdnd;
+
+USE lairdnd;
+
+CREATE TABLE reviews (
+  /*
+   * id 
+   * offering
+   * guest
+   * date
+   * owner
+   * comment
+   * reply
+   * ratings
+   */
+);
+
+CREATE TABLE offerings (
+  /*
+   * id
+   * title
+   */
+);
+
+CREATE TABLE guests (
+  /* 
+   * id
+   * name
+   */
+);
+
+CREATE TABLE owners (
+  /*
+   * id 
+   * name
+   */
+);
