012e009936ac90d02aaacf6ec986bc1276f6a399
Tim Luu <timbo.luu@gmail.com>
2019-02-15 11:23:22 -0800

    Add styles for ratings section

---
+++ client/ratingStyles.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Star } from 'styled-icons/material/Star';
+import { StarHalf } from 'styled-icons/material/StarHalf';
+import { StarBorder } from 'styled-icons/material/StarBorder';
+import styled from 'styled-components';
+
+export const Top = styled.div`
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+`
+
+export const GreenStar = styled(Star)`
+  color: #00A699;
+`
+
+export const GreenStarHalf = styled(StarHalf)`
+  color: #00A699;
+`
+
+export const GreenStarBorder = styled(StarBorder)`
+  color: #00A699;
+`
