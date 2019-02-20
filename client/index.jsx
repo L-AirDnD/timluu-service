5ecfe006b0029f8570b224250a446c8aa3bfc70b
Tim Luu <timbo.luu@gmail.com>
2019-02-19 19:10:30 -0800

    Remove rendering from module
    Move the class to the window to be rendered
    in the proxy server.

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Review from './src/Review.jsx';
 
-ReactDOM.render(<Review />, document.getElementById('review'));
+window.Review = Review;
